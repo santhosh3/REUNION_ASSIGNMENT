dc26b4eea324381c806ba0943749df2bc7a4a15b
santhosh3 <santhoshchinna109@gmail.com>
2022-09-10 19:49:39 +0530

    a

--- src/index.js
+++ src/index.js
@@ -12,7 +12,13 @@ mongoose.connect(process.env.CLUSTER,{useNewUrlParser: true})
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
+ 
 app.use('/', route);
+
+app.get('/',(req,res) => {
+    res.send('welocome')
+})
+
 
 app.listen(process.env.PORT, function () {
     console.log('Express app running on port ' + (process.env.PORT))
